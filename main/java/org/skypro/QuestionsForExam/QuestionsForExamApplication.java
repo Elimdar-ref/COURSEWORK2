f0b782ddaf66db1df36dd590278749045ca363b5
Admin <elimdar.ref@mail.ru>
2025-08-15 10:01:00 +0300

    COURSEWORK

---
+++ main/java/org/skypro/QuestionsForExam/QuestionsForExamApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.QuestionsForExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsForExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsForExamApplication.class, args);
+	}
+
+}
